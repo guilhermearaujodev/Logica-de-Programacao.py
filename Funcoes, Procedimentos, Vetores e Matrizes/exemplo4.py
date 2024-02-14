dd807586c67b604baa4b71f87b4dc4ac7c6d73ed
Guilherme <guilherme.sajs@gmail.com>
2024-02-13 22:38:48 -0300

    Função sem Parâmetro A mecânica de funcionamento de função e procedimento é quase idêntica. A diferença  é  que uma  função retorna  um  valordo subalgoritmo (com  o  comando return) para o algoritmo que o chamou e este programa utiliza para algum fim este valor retornado.

---
+++ Funcoes, Procedimentos, Vetores e Matrizes/exemplo4.py
@@ -0,0 +1,11 @@
+#Função sem Parâmetro
+#Criar uma função do tipo real que retorne o valor de pi
+#Para calcular a área de um circulo com o raio 5 (a = pi . r2).  
+
+#Definindo uma função
+def pi():
+    return 3.14159
+
+r = 5 
+a = pi() * r ** 2
+print("A área do círculo com raio", r, "é", a)
